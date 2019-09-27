a840efe48bae6283494ef01ea9bc0d0042b66805
Amir Yunas <ayunas@gmail.com>
2019-09-27 08:26:27 -0500

    changed migrations and seeds of restaurants table.  solution was to run migrations on heroku again.

--- data/migrations/20190921095518_restaurants.js
+++ data/migrations/20190921095518_restaurants.js
@@ -2,15 +2,15 @@
 exports.up = function(knex) {
     return knex.schema.createTable('restaurants', tbl => {
         tbl.increments()
-        tbl.text('restaurant_name',128).notNullable().unique();
-        tbl.text('contact',32);
-        tbl.text('address', 128);
-        tbl.text('city', 32);
-        tbl.text('zip', 9);
-        tbl.text('phone',10);
-        tbl.text('email',32);
-        tbl.text('username', 32).notNullable().unique();
-        tbl.text('password').notNullable();
+        tbl.string('restaurant_name',128).notNullable().unique();
+        tbl.string('contact',32);
+        tbl.string('address', 128);
+        tbl.string('city', 32);
+        tbl.string('zip', 9);
+        tbl.string('phone',10);
+        tbl.string('email',32);
+        tbl.string('username', 32).notNullable().unique();
+        tbl.string('password').notNullable();
     })
 };
 
--- data/seeds/004-restaurants.js
+++ data/seeds/004-restaurants.js
@@ -2,9 +2,9 @@ const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex) {
       return knex('restaurants').insert([
-        {restaurant_name: 'Bread and Kabob', contact : 'suzie', address: '999 driver lane', city: 'madina', zip: "12345", phone: '123-456-7890', email: 'res@email.com', username: 'bk', password: bcrypt.hashSync('pass', 10)},
-        {restaurant_name: 'Pizza Hut', contact : 'jake',  address: '950 sirat way', city: 'makkah', zip: "12345", phone: '123-456-7890', email: 'res2@email.com', username: "pizzahut", password: bcrypt.hashSync('pass', 10)},
-        {restaurant_name: 'Applebees', contact : 'franchi', address: '666 highway blvd', city: 'jerusalem', zip: "12345", phone: '123-456-7890', email: 'res3@email.com', username: "applebees", password: bcrypt.hashSync('pass', 10)}
+        {id: 1, restaurant_name: 'Bread and Kabob', contact : 'suzie', address: '999 driver lane', city: 'madina', zip: "12345", phone: '123-456-7890', email: 'res@email.com', username: 'bk', password: bcrypt.hashSync('pass', 10)},
+        {id: 2, restaurant_name: 'Pizza Hut', contact : 'jake',  address: '950 sirat way', city: 'makkah', zip: "12345", phone: '123-456-7890', email: 'res2@email.com', username: "pizzahut", password: bcrypt.hashSync('pass', 10)},
+        {id: 3, restaurant_name: 'Applebees', contact : 'franchi', address: '666 highway blvd', city: 'jerusalem', zip: "12345", phone: '123-456-7890', email: 'res3@email.com', username: "applebees", password: bcrypt.hashSync('pass', 10)}
       ]);
 };
 
